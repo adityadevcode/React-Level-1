2182a1002d8ddc5f9758d47dce705ff8f587b485
addycoderepo <adityagitcode@gmail.com>
2022-09-08 10:17:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import Technologies from './Technologies';
+import Form from './Form';
+import Client from './Client';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>This is Appjs</h2>
+      <Home/>
+      <Technologies/>
+      <Form/>
+      <Client/>
     </div>
   );
 }
